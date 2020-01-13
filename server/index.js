017e425f0ac569144b09d9afaae7167bd2bfee1a
Bryan <paolo186@gmail.com>
2020-01-13 08:34:06 -0800

    added mongooseJS; connected to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 require('./services/passport');
 require('./routes/authRoutes')(app);
 
+// connect to MongoDB
+mongoose.connect(
+    process.env.MONGO_URI,
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    },
+);
+
+
+// start server
 const PORT = process.env.PORT || 5000;
 console.log("listening at port", PORT)
 app.listen(PORT);
