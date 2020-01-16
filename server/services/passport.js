035fdd9ae71765c7fed7688d9a1c9b3457d2cad2
Bryan <paolo186@gmail.com>
2020-01-16 08:16:00 -0800

    implemented passport's serializeUser to be used for set-cookie

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,23 @@ const mongoose = require('mongoose');
 // with only 1 arg, this is to retrieve a collection called 'users'
 const User = mongoose.model('users');
 
+// after user login => used for set-cookie sent back to client to be used for future server requests 
+passport.serializeUser((user, done) => {
+  // user.id is MongoDB's auto-generated ID for the user
+  done(null, user.id); // 1st arg is for errors
+});
+
+// 1st arg in callback is whatever object we serialized above (in this case, user.id)
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+  .then(user => {
+    done(null, user);
+  })
+  .catch((error, info) => {
+    console.log(error, info);
+  })
+});
+
 // register GoogleStrategy to passport module; used for Google OAuth 2.0;
 // registered OAuth redirect callback in Google API
 // http://localhost:5000/auth/google/callback
@@ -28,8 +45,14 @@ passport.use(
             // creates and saves this new User object to MongoDB
             new User({ googleId: profile.id })
               .save()
-              .then(newUser => done(null, newUser));
+              .then(newUser => done(null, newUser))
+              .catch((error, info) => {
+                console.log(error, info);
+              })
           }
+        })
+        .catch((error, info) => {
+          console.log(error, info);
         })
 
     })
